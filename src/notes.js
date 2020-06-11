c4fe8e266716a3426f2147140b8ee64120b1724c
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2020-06-11 13:08:34 +0530

    First Commit

---
+++ src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+    {
+        key: 1,
+        title: "Delegation",
+        content:
+            "Q. How many programmers does it to change a light bulb?. A. None - It's a hardware problem."
+    },
+
+    {
+        key: 2,
+        title: "Loops",
+        content:
+            "How to keep a programmer in the shower forever. Show him the shampoo bottle instruction. Lather. Rinse. Repeat."
+    },
+
+    {
+        key: 3,
+        title: "Arrays",
+        content:
+            "Q. What did the programmer quit his job? Because he did didn't get the array."
+    }
+]
+
+export default notes;
